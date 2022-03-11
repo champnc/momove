3a7355e8e1cc72a4dea627ce8ea990e4a6876554
champnc <natdanai.dev@gmail.com>
2022-03-11 23:50:42 +0700

    add theme

---
+++ lib/config/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primaryColor: const Color(0xFF00B0FF),
+    primaryColorDark: const Color(0xFF0091EA),
+    primaryColorLight: const Color(0xFF40C4FF),
+
+    
+    );
+}
