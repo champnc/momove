f1f616c5cc8e35232c09d9f497ac632a33f4a150
champnc <natdanai.dev@gmail.com>
2022-03-12 12:47:58 +0700

    add bottom navigation

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = "/";
 
   static Route route() {
@@ -13,9 +13,60 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _selected = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: const Center(
+        child: Text("Home"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Theme.of(context).primaryColorDark,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selected,
+        onTap: (int selectedIndex) {
+          setState(() {
+            _selected = selectedIndex;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: const Icon(Icons.home),
+            tooltip: "Home",
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: const Icon(Icons.home),
+            tooltip: "Home",
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: const Icon(Icons.home),
+            tooltip: "Home",
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: const Icon(Icons.home),
+            tooltip: "Home",
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+        ],
+      ),
     );
   }
 }
