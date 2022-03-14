11a3087303556b2beb64145dd60bee27d3d87910
champnc <natdanai.dev@gmail.com>
2022-03-15 00:14:00 +0700

    move color to constant

---
+++ lib/config/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF8888E0);
+const kPrimaryColorDark = Color(0xFF4B4BD0);
+const kPrimaryColorLight = Color(0xFFB0B0EB);
--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:momove/config/constant.dart';
 
 ThemeData theme(BuildContext context) {
   return ThemeData(
-    primaryColor: const Color(0xFF8888E0),
-    primaryColorDark: const Color(0xFF4B4BD0),
-    primaryColorLight: const Color(0xFFB0B0EB),
+    primaryColor: kPrimaryColor,
+    primaryColorDark: kPrimaryColorDark,
+    primaryColorLight: kPrimaryColorLight,
 
     textTheme: GoogleFonts.varelaRoundTextTheme(
       Theme.of(context).textTheme, // If this is not set, then ThemeData.light().textTheme is used.
